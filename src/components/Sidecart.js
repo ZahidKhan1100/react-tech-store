a2273c00e87185f81f8401bdbc7a92da61d75cd1
ZahidKhan1100 <kzahid416@gmail.com>
2020-05-16 03:31:54 +0500

    sidecart added

--- src/components/Sidecart.js
+++ src/components/Sidecart.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import {ProductConsumer} from '../context';
 import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
 
 export default function Sidecart() {
     return (
         <ProductConsumer>
             {
-               value =>{ const {cartOpen , closeCart, cart} = value;
+               value =>{ const {cartOpen , closeCart, cart,cartTotal} = value;
                return <CartWrapper show={cartOpen} onClick={closeCart}>
-                   <p>cart items</p>
+                   <ul>
+                       {cart.map(item=>{
+                           return(
+                           <li key={item.id} className="cart-item mb-4">
+                               <img width="35" src={`../${item.image}`} alt="cart item"></img>
+                                <div className="mt-3">
+                                    <h6 className="text-uppercase">{item.title}</h6>
+                                    <h6 className="text-title text-capitalize">amount:{item.count}</h6>
+
+                                </div>
+                           </li>
+                           )
+                       })}
+                   </ul>
+                   <h4 className="text-capitalize text-main">cart total:${cartTotal}</h4>
+                   <div className="text-center my-5">
+                       <Link to="/cart" className="main-link">
+                           cart page
+                       </Link>
+                   </div>
                </CartWrapper>
             }}
         </ProductConsumer>
@@ -28,5 +49,15 @@ transition: all 0.5s ease-in-out;
 transform:${props => (props.show?'translateX(0)':'translateX(100%)')};
 @media (min-width:576px){
     width:20rem;
+}
+overflow:scroll;
+padding: 2rem;
+
+ul{
+    padding: 0 !important;
+}
+
+.cart-item{
+    list-style-type:none;
 }
 `
