ece786db82e0c07bd74583bfc872ef35b04a5342
ZahidKhan1100 <kzahid416@gmail.com>
2020-05-18 22:46:20 +0500

    completed

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {ProductConsumer} from '../../context';
 import Title from '../Title';
 import Product from '../Product';
+import ProductFiltered from './ProductFiltered';
 
 export default function ProductPage() {
     return (
@@ -12,11 +13,31 @@ export default function ProductPage() {
                     <div className="container">
                         {/* {Title} */}
                         <Title center title="our products"></Title>
+                        {/* {product filtered} */}
+                        <ProductFiltered></ProductFiltered>
+                        
+                        {/* {total counts} */}
+                        <div className="row">
+                            <div className="col-10 mx-auto">
+                                <h6 className="text-title">
+                                    total products : {filteredProducts.length}
+                                </h6>
+                            </div>
+                        </div>
                         {/* {Products} */}
+                        
                         <div className="row py-5">
-                            {filteredProducts.map(product =>{
-                                return <Product key={product.id} product={product}></Product>
-                            })}
+                            {filteredProducts.length === 0 ? (
+                                <div className="col text-title text-center">
+
+                                </div>
+                            ):(
+                    
+                        
+                            filteredProducts.map(product =>{
+                                return <Product key={product.id} product={product}></Product>;
+                            })
+                            )}
 
                         </div>
 
